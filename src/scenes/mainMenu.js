821f513d05f08e5eefefaac06ce87bd9673d96d9
Oleh Kinchevskyi <Oleh.Kinchevskyi@gmail.com>
2024-12-16 15:16:35 +0200

    platform movement

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -35,7 +35,15 @@ export default function mainMenu() {
             bgPieces.push(bgPieces.shift());
         }
 
-        bgPieces[0].move(-4000, 0);
+        bgPieces[0].move(-100, 0);
         bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+
+        if (platforms[1].pos.x < 0) {
+            platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4, 450);
+            platforms.push(platforms.shift());
+    }
+
+        platforms[0].move(-4000, 0);
+        platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450);
     });
 }
