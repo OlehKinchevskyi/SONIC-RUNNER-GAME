3910b18f8621a9a91bcac709f80029d2fc22f47d
Oleh Kinchevskyi <Oleh.Kinchevskyi@gmail.com>
2024-12-28 19:40:22 +0200

    enemy logic

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,5 +1,6 @@
 import k from "../kaplayCtx";
 import { makeSonic } from "../entities/sonic";
+import { makeMotobug } from "../entities/motobug";
 
 export default function game() {
     k.setGravity(3100);
@@ -23,11 +24,48 @@ export default function game() {
     const sonic = makeSonic(k.vec2(200, 745));
     sonic.setControls();
     sonic.setEvents();
+    sonic.onCollide("enemy", () => {
+        if (!sonic.isGrounded()) {
+            k.play("destroy", { volume: 0.5 });
+            k.play("hyper-ring", { volume: 0.5 });
+            k.destroy(enemy);
+            sonic.play("jump");
+            sonic.jump();
+            return;
+        }
+
+        k.play("hurt", { volume: 0.5 });
+
+        k.go("gameover");
+    })
+
+    
 
     let gameSpeed = 300;
     k.loop(1, () => {
         gameSpeed += 50;
     });
+
+    const spawmMotobug = () => {
+        const motobug = makeMotobug(k.vec2(1950, 773));
+        motobug.onUpdate(() => {
+            if (gameSpeed < 3000) {
+                motobug.move(-(gameSpeed + 300), 0);
+                return;
+            }
+
+            motobug.move(-gameSpeed, 0);
+        });
+
+        motobug.onExitScreen(() => {
+            if (motobug.pos.x < 0) k.destroy(motobug);
+        });
+
+        const waitTime = k.rand(0.5, 2.50);
+        k.wait(waitTime, spawmMotobug);
+    };
+
+    spawmMotobug();
 
     k.add([
         k.rect(1920, 300),
